a14188854622c52d081dc348b1c30c388c262906
unknown <rajatkjha@outlook.com>
2020-05-17 23:01:29 +0530

    Taking input by argumnents

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+import sys
 import re
+
+print(type(sys.argv))
+
 regex = '''^(25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.( 
             25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.( 
             25[0-5]|2[0-4][0-9]|[0-1]?[0-9][0-9]?)\.( 
@@ -10,14 +14,14 @@ def check(Ip):
 	else:
 		return False;
 
-print ("Enter the IP address:")
-ipa = input()
-while(True):
-	if(check(ipa)):
-		break;
-	else:
-		print ("Kindly enter correct IP address:")
-		ipa=input()
+# print ("Enter the IP address:")
+ipa = sys.argv
+# while(True):
+# 	if(check(ipa)):
+# 		break;
+# 	else:
+# 		print ("Kindly enter correct IP address:")
+# 		ipa=input()
 
 print (ipa)
 
